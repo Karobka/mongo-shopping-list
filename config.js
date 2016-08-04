51ebb8c22d392640e974a8affec8d945ee0862c3
TimW <mithgirw@gmail.com>
2016-08-04 22:15:04 +0000

    trying to add mLab address

--- config.js
+++ config.js
@@ -7,10 +7,12 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
                        
-                            'mongo ds139985.mlab.com:39985/mongo-shopping-list -u <dbuser> -p <dbpassword>'
-                            //'mongodb://localhost/shopping-list' :
-                            //'mongodb://localhost/shopping-list-dev');
+                            'mongodb://ds139985.mlab.com:39985/mongo-shopping-list'
+                        );
+                            /**'mongodb://localhost/shopping-list' :
+                            //'mongodb://localhost/shopping-list-dev'
+                            );
     //Specify port with environment variable.
-exports.PORT = process.env.PORT || 8080;
+//exports.PORT = process.env.PORT || 8080;
 
 //mongo ds139985.mlab.com:39985/mongo-shopping-list -u <dbuser> -p <dbpassword>
